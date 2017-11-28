e830cf730d1250c7822d64ec490b73d6dd137611
Ice Zero <ziv3@outlook.com>
2017-11-28 15:48:08 +0800

    CHANGE: Floders

---
+++ js/basic/bubble-sort.js
@@ -0,0 +1,13 @@
+'use strict';
+
+Array.prototype.BubbleSort = function ( = false) {
+  if (setOrder) {
+    for (let i = 0; i < this.length; ++i) {
+      this[i] 
+    }
+  }
+  return this;
+};
+
+
+[1, 2, 3].BubbleSort();
