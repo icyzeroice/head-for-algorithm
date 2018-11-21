15613bbc2f02fc9f08d4e6dfe713659d2caafda3
icyzeroice <ziv3@outlook.com>
2018-11-21 22:20:38 +0800

    :sweat_drops: little fix

--- basic/cpp/Graph/Graph.cpp
+++ basic/cpp/Graph/Graph.cpp
@@ -7,49 +7,74 @@ private:
 
 
 public:
+
+  /**
+   * # 图的几种表示方法：
+   * 
+   * ### 邻接矩阵
+   * 
+   *   使用 V * V 的布尔矩阵
+   *   *空间*: V * V
+   *   *缺点*: 在图的顶点多时，所需空间太大
+   * 
+   * ### 边的数组
+   * 
+   *   *缺点*: 不能很快地实现度的计算
+   * 
+   * ### 邻接表数组
+   * 
+   *   *空间*: V + E
+   *   每个顶点都有一个以相邻顶点为元素的数组
+   *   
+   *   例： 用 Bag 实现数组
+   * 
+   * ### 邻接集
+   *  
+   *   用符号表代替数组
+   *   *优点*：这样就顶点就不用必须是整数型了
+   * 
+   * 
+   */
   Graph(int V);
   Graph(char *c);
   ~Graph();
 
-  int V();                      // get the amount of vertexes/vertices
-  int E();                      // get the amount of edges
-  void addEdge(int v, int w);   // add edge `v-w`
-  int *adj(int v);              // vertexes next to `v`
+  int V();                                       // get the amount of vertexes/vertices
+  int E();                                       // get the amount of edges
+  void addEdge(int v, int w);                    // add edge `v-w`
+  int *adj(int v);                               // vertexes next to `v`
     
   static int degree(Graph G, int v);
   static int maxDegree(Graph G);
   static int minDegree(Graph G);
   static int avgDegree(Graph G);
 
+  static bool isMarked(Graph G, int s, int v);   // `s` is connected with `v`
+  static int countUnion(Graph G, int s);         // amount of all the vertexes that are connected with `s`
+
   static int numOfSelfLoops(Graph G);
 
-  void print();                 // print via Adjacency List style
-};
+  /**
+   * # 图的查找
+   * 
+   * ### DFS (Deep First Search)
+   * 
+   *   时间复杂度： 所有顶点的度数之和
+   * 
+   * - 单向通道
+   * - 使用 LIFO
+   * 
+   * ### BFS (Broad First Search)
+   * 
+   *   时间复杂度: 最坏 V + E 的正比
+   * 
+   *   - 使用 FIFO
+   * 
+   */
+  static void dfs(Graph G, int s);
+  static void bfs(Graph G, int s);
 
-/**
- * # 图的几种表示方法：
- * 
- * ### 邻接矩阵
- * 
- *   使用 V * V 的布尔矩阵
- *   *空间*: V * V
- *   *缺点*: 在图的顶点多时，所需空间太大
- * 
- * ### 边的数组
- * 
- *   *缺点*: 不能很快地实现度的计算
- * 
- * ### 邻接表数组
- * 
- *   *空间*: V + E
- *   每个顶点都有一个以相邻顶点为元素的数组
- *   
- *   例： 用 Bag 实现数组
- * 
- * ### 邻接集
- *  
- *   用符号表代替数组
- *   *优点*：这样就顶点就不用必须是整数型了
- */
+  void print();                                  // print via Adjacency List style
+};
 
 
